f173f5f56d871c5e0fdf4a7899df897df8801af6
toashel <shelchean@gmail.com>
2017-02-13 14:36:56 +1300

    Add api routes to poetryDB

---
+++ app_api/routes/api.js
@@ -0,0 +1,71 @@
+var router = require('express').Router();
+var request = require('request');
+// var poetryController = require('poetryController');
+
+/* Route to get all titles of poems */
+router.get('/titles', function(req,res) {
+	var options = {
+		method: 'get',
+		uri: 'http://poetrydb.org/title',
+		json: true
+	};
+
+	request(options, function(err, response, body) {
+		if (err)
+			throw err;
+		res.json(body);
+	});
+
+});
+
+/* Route to get the exact poem from a title 
+   Is case sensitive.
+*/
+router.get('/titles/:title', function(req, res) {
+	var options = {
+		method: 'get',
+		uri: 'http://poetrydb.org/title/' + req.params.title + ':abs',
+		json: true
+	};
+
+	request(options, function(err, response, body) {
+		if (err)
+			throw err;
+		res.json(body);
+	});
+});
+
+/* Route to get all authors of poems */
+router.get('/authors', function(req,res) {
+	var options = {
+		method: 'get',
+		uri: 'http://poetrydb.org/author',
+		json: true
+	};
+
+	request(options, function(err, response, body) {
+		if (err)
+			throw err;
+		res.json(body);
+	});
+
+});
+
+/* Route to get the exact list of poems from an author
+   Is case sensitive.
+*/
+router.get('/authors/:author', function(req,res) {
+	var options = {
+		method: 'get',
+		uri: 'http://poetrydb.org/author/' + req.params.author + ':abs/title',
+		json: true
+	};
+
+	request(options, function(err, response, body) {
+		if (err)
+			throw err;
+		res.json(body);
+	});
+
+});
+module.exports = router;
