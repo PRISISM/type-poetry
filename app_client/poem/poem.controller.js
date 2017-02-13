f2494d43a4dbcc4a29ca24d6e58b1dcbec0c01b4
toashel <shelchean@gmail.com>
2017-02-14 12:58:34 +1300

    Fix git error'

--- app_client/poem/poem.controller.js
+++ app_client/poem/poem.controller.js
@@ -10,21 +10,34 @@
 		var title = $routeParams.title;
 		$window.document.title = title;
 
-		vm.typedPoem = [];
-
 		$http.get('/api/titles/' + title).then(function successCallback(result) {
 			vm.poem = result.data[0];
+			vm.typedPoem = Array(vm.poem.lines.length);
+			vm.poemIndex = 0;
 			console.log(result);
 		}, function errorCallback(err) {
 			console.log(err);
 			// handle err
 		});
 
+		/* Compares the current string to the respective line in the poem array */
+		vm.check = function (typed, index) {
+			if (typed == vm.poem.lines[index]) {
+				vm.next();
+			}
+		};
+
+		/* Increments the poem index -- on last should trigger a modal/end. */
+		vm.next = function() {
+			if (vm.poemIndex >= vm.poem.lines.length - 1) {
+				vm.poemIndex = 0;
+			}
+			else {
+				vm.poemIndex++;
+			}
+		};
+
 		console.log($routeParams.title);
 
-		// $http.get('/api/titles').then(function(result) {
-		// 	console.log(result);
-		// 	vm.titles = result.data.titles;
-		// });
 	}
 })();
